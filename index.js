450edcf3a10d8de42ea3e5a5608a1b56d5cde34b
Ethan Warden <ethancwarden@gmail.com>
2024-10-10 15:45:49 -0400

    10/10 EW: finished project

--- index.js
+++ index.js
@@ -15,9 +15,17 @@ let massStr = ""
 btnEl.addEventListener("click", function(){
     
     let feetConv = parseFloat(inputEl.value) * 3.28084
-    let metersConv = parseFloat(inputEl.value) / 3.0
+    let metersConv = parseFloat(inputEl.value) * 0.3048
+
+    let gallonsConv = parseFloat(inputEl.value) * 0.264172
+    let litersConv = parseFloat(inputEl.value) * 3.78541
+
+    let poundsConv = parseFloat(inputEl.value) * 0.453592
+    let kilosConv = parseFloat(inputEl.value) *  2.20462
 
     lengthStr = `${inputEl.value} meters = ${feetConv.toFixed(3)} feet | ${inputEl.value} feet = ${metersConv.toFixed(3)} meters`
+    volumeStr = `${inputEl.value} liters = ${gallonsConv.toFixed(3)} gallons | ${inputEl.value} gallons = ${litersConv.toFixed(3)} liters`
+    massStr = `${inputEl.value} kilos = ${poundsConv.toFixed(3)} pounds | ${inputEl.value} pounds = ${kilosConv.toFixed(3)} kilos`
 
     lengthEl.innerHTML = lengthStr
     volumeEl.innerHTML = volumeStr
